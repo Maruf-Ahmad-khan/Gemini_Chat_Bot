923234668182e7f7e2abc0a0e5b1a0ebc8072911
Maruf <mk7446683@gmail.com>
2024-12-06 15:51:01 +0530

    First

--- Cuelinks.py
+++ Cuelinks.py
@@ -8,10 +8,28 @@ st.set_page_config(
 )
 
 # Add title
-st.title("Power BI Dashboard Integration")
+st.title("Cuelinks Report")
 
 # Power BI Embed URL
 power_bi_embed_url = "https://app.fabric.microsoft.com/reportEmbed?reportId=c71a2be3-ad0c-4c17-ac14-a6895c4e7bc4&autoAuth=true&ctid=4a36bb2f-1c55-49e0-9a23-6281dfd38c6b"
+
+# HTML code to embed Power BI in an iframe
+html_code = f"""
+<iframe 
+    title="Power BI Dashboard" 
+    width="100%" 
+    height="800"  # Increased height for better viewing
+    src="{power_bi_embed_url}" 
+    frameborder="0" 
+    allowFullScreen="true">
+</iframe>
+"""
+
+# Render the iframe in Streamlit
+components.html(html_code, height=800)  # Match the iframe height here
+
+st.title("Aliexpress and Flipshope(Aug, Sept And Nov) Report")
+power_bi_embed_url = "https://app.powerbi.com/reportEmbed?reportId=269f99fa-f477-4ce4-968c-8dc43b1c1959&autoAuth=true&ctid=4a36bb2f-1c55-49e0-9a23-6281dfd38c6b"
 
 # HTML code to embed Power BI in an iframe
 html_code = f"""
